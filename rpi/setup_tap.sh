67cddb257ea29f79f86f8ffb1a8317eb7d753e41
Trey Bigsby <treybigsby@gmail.com>
2020-12-06 21:21:45 -0700

    Final adjustments to tap networking script

--- rpi/setup_tap.sh
+++ rpi/setup_tap.sh
@@ -1,8 +1,11 @@
 sudo brctl addbr br0
 sudo ip addr flush dev enp0s3
 sudo brctl addif br0 enp0s3
-sudo tunctl -u $(whoami)
+sudo tunctl -t tap0 -u `whoami`
 sudo brctl addif br0 tap0
-sudo ip link set dev br0 up
-sudo ip link set dev tap0 up
-sudo dhclient br0
+sudo dhclient -v br0
+sudo ifconfig enp0s3 up
+sudo ifconfig tap0 up
+sudo ifconfig br0 up
+sudo bash -c 'echo "1" > /proc/sys/net/ipv4/ip_forward'
+sudo iptables -t nat -A POSTROUTING -o br0 -j MASQUERADE
